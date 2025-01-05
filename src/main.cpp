04e28e3501b20801dcab055983a87b8e8d3cb13e
changmx <changmx@impcas.ac.cn>
2025-01-05 17:27:00 +0800

    feat：增加束流vector的创建

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "general.h"
+#include "particle.h"
 #include "parameter.h"
 
 int main(int argc, char** argv)
@@ -8,7 +9,20 @@ int main(int argc, char** argv)
 	print_logo();
 	print_copyright();
 
-	Parameter Para(argc, argv);
+	Parameter para(argc, argv);
 
-	print_beam_and_bunch_parameter(Para);
+	std::vector<Bunch> beam0;
+	std::vector<Bunch> beam1;
+	for (size_t i = 0; i < para.Nbunch[0]; i++)
+	{
+		Bunch bunch;
+		beam0.push_back(bunch);
+	}
+	for (size_t i = 0; i < para.Nbunch[1]; i++)
+	{
+		Bunch bunch;
+		beam0.push_back(bunch);
+	}
+
+	print_beam_and_bunch_parameter(para);
 }
