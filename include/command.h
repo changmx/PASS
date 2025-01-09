ba7d6db2679c2f1d009e2d229e8766e29affde15
changmx <changmx@impcas.ac.cn>
2025-01-09 21:40:07 +0800

    feat：callCuda等宏中输出cudaGetErrorString(cudaGetLastError())而非err这一表示真假的信息；把spdlog放在函数中而非参数列表

--- include/command.h
+++ include/command.h
@@ -19,7 +19,7 @@ public:
 	virtual void execute() = 0;
 
 	double s = -1;
-	std::string name;
+	std::string name = "empty";
 protected:
 
 private:
@@ -43,76 +43,76 @@ private:
 //};
 
 
-inline void checkCudaError(cudaError_t err, const char* file, int line, std::shared_ptr<spdlog::logger>& logger)
+inline void checkCudaError(cudaError_t err, const char* file, int line)
 {
 	if (err != cudaSuccess)
 	{
 		//std::cerr << "\nError: cuda error: " << cudaGetErrorString(cudaGetLastError()) << ", in " << file << ", at line " << line << "\n";
-		logger->error("cuda error: {}, in {}, at line {}", err, file, line);
+		spdlog::get("logger")->error("cuda error: {}, in {}, at line {}", cudaGetErrorString(cudaGetLastError()), file, line);
 		cudaDeviceReset();
 		exit(EXIT_FAILURE);
 	}
 }
-inline void checkCufftError(cufftResult err, const char* file, int line, std::shared_ptr<spdlog::logger>& logger)
+inline void checkCufftError(cufftResult err, const char* file, int line)
 {
 	if (err != CUFFT_SUCCESS)
 	{
 		//std::cerr << "\nError: cufft error number: " << err << ", in " << file << ", at line " << line << "\n";
-		logger->error("cufft error: {}, in {}, at line {}", err, file, line);
+		spdlog::get("logger")->error("cufft error: {}, in {}, at line {}", cudaGetErrorString(cudaGetLastError()), file, line);
 		cudaDeviceReset();
 		exit(EXIT_FAILURE);
 	}
 }
 
-inline void checkCusparse(cusparseStatus_t err, const char* file, int line, std::shared_ptr<spdlog::logger>& logger)
+inline void checkCusparse(cusparseStatus_t err, const char* file, int line)
 {
 	if (err != CUSPARSE_STATUS_SUCCESS)
 	{
 		//std::cerr << "\nError: cusparse error number: " << err << ", in " << file << ", at line " << line << "\n";
-		logger->error("cufft cusparse: {}, in {}, at line {}", err, file, line);
+		spdlog::get("logger")->error("cufft cusparse: {}, in {}, at line {}", cudaGetErrorString(cudaGetLastError()), file, line);
 		cudaDeviceReset();
 		exit(EXIT_FAILURE);
 	}
 }
 
-inline void checkCusolver(cusolverStatus_t err, const char* file, int line, std::shared_ptr<spdlog::logger>& logger)
+inline void checkCusolver(cusolverStatus_t err, const char* file, int line)
 {
 	if (err != CUSOLVER_STATUS_SUCCESS)
 	{
 		//std::cerr << "\nError: cusolver error number: " << err << ", in " << file << ", at line " << line << "\n";
-		logger->error("cusolver cusparse: {}, in {}, at line {}", err, file, line);
+		spdlog::get("logger")->error("cusolver cusparse: {}, in {}, at line {}", cudaGetErrorString(cudaGetLastError()), file, line);
 		cudaDeviceReset();
 		exit(EXIT_FAILURE);
 	}
 }
 
-inline void checkCublas(cublasStatus_t err, const char* file, int line, std::shared_ptr<spdlog::logger>& logger)
+inline void checkCublas(cublasStatus_t err, const char* file, int line)
 {
 	if (err != CUBLAS_STATUS_SUCCESS)
 	{
 		//std::cerr << "\nError: cublas error number: " << err << ", in " << file << ", at line " << line << "\n";
-		logger->error("cublas cusparse: {}, in {}, at line {}", err, file, line);
+		spdlog::get("logger")->error("cublas cusparse: {}, in {}, at line {}", cudaGetErrorString(cudaGetLastError()), file, line);
 		cudaDeviceReset();
 		exit(EXIT_FAILURE);
 	}
 }
 
 #ifndef callCuda
-#define callCuda(call, logger) (checkCudaError(call, __FILE__, __LINE__, logger));
+#define callCuda(call) (checkCudaError(call, __FILE__, __LINE__));
 #endif // !callCuda
 
 #ifndef callCufft
-#define callCufft(call, logger) (checkCufftError(call, __FILE__, __LINE__, logger));
+#define callCufft(call) (checkCufftError(call, __FILE__, __LINE__));
 #endif // !callCufft
 
 #ifndef callCusparse
-#define callCusparse(call, logger) (checkCusparse(call, __FILE__, __LINE__, logger));
+#define callCusparse(call) (checkCusparse(call, __FILE__, __LINE__));
 #endif // !callCusparse
 
 #ifndef callCusolver
-#define callCusolver(call, logger) (checkCusolver(call, __FILE__, __LINE__, logger));
+#define callCusolver(call) (checkCusolver(call, __FILE__, __LINE__));
 #endif // !callCusolver
 
 #ifndef callCublas
-#define callCublas(call, logger) (checkCublas(call, __FILE__, __LINE__, logger));
+#define callCublas(call) (checkCublas(call, __FILE__, __LINE__));
 #endif // !callCublas
