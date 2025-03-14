ee147e76164dc5fab7b584e234414943574710a6
changmx <changmx@impcas.ac.cn>
2025-03-14 17:08:12 +0800

    fix：修正注入点位置判断

--- src/injection.cu
+++ src/injection.cu
@@ -39,7 +39,7 @@ Injection::Injection(const Parameter& para, int input_beamId, const Bunch& Bunch
 	{
 		s = data.at("Sequence").at("Injection").at("S (m)");
 		name = data.at("Sequence").at("Injection").at("Command");
-		if (abs(s) < 1e-10)
+		if (abs(s) > 1e-10)
 		{
 			spdlog::get("logger")->error("[Injection] The position of injection point (simulation start point) should be 0, but now is : {}.", s);
 			std::exit(EXIT_FAILURE);
