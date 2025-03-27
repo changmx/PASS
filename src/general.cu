59f02cf0940bfc5375aab5f3113395fcb68be1e2
changmx <changmx@impcas.ac.cn>
2025-03-27 21:23:02 +0800

    feat：将读取sequence输入参数的函数移动到其他文件中

--- src/general.cu
+++ src/general.cu
@@ -1,5 +1,4 @@
 #include "general.h"
-#include "injection.h"
 
 #include <ctime>
 #include <iomanip>
@@ -407,68 +406,3 @@ void show_device_info() {
 }
 
 
-void read_simulation_config(const Parameter& Para, const std::vector<Bunch>& beam, int input_beamId, std::vector<Command*>& command_vec) {
-
-	if (1 == Para.Nbeam && 1 == input_beamId)
-	{
-		return;	// if we only have one beam, we don't need to read the json file of beam1. The size of beam1 vector will be zero.
-	}
-
-	using json = nlohmann::json;
-	std::ifstream jsonFile(Para.path_input_para[input_beamId]);
-	json data = json::parse(jsonFile);
-
-	/*std::string a = "Sequence";
-	double s = data.at(a).at("Injection").at("S (m)");
-	std::cout << "s = " << s << ", " << typeid(a).name() << std::endl;*/
-
-	for (auto item = data.at("Sequence").begin(); item != data.at("Sequence").end(); item++) {
-
-		try
-		{
-			/******
-			Key is the keyword that indicates the types of simulation module
-			Now, we have these keys (modules):
-				Injection: generate particles of a bunch in one turn or multi turns
-				Transfer: transfer particles of a bunch from s0 to s1
-				SpaceCharge: perform space charge simulation at specified position
-				BeamBeam: perform beam-beam simulation at specified position
-				Wakefield: perform wakefield simulation at specified position
-				RFCavity: perform acceleration or logituninal phase space manipulation
-				StatMonitor: calculate and save bunch's information like centroid, size, emittance ...
-				LumiMonitor: calculate and save collision luminosity
-				DistMonitor: save bunch's distribution
-				PhaseMonitor: save particle's phase advance for tune spread analysis, FMA analysis ...
-			*******/
-
-			std::string ikey = std::string(item.key());
-
-			if ("Injection" == ikey)
-			{
-				for (size_t i = 0; i < Para.Nbunch[input_beamId]; i++)
-				{
-					Injection* inj = new Injection(Para, input_beamId, beam[i]);
-					Command* command = new InjectionCommand(inj);
-					command_vec.push_back(command);
-				}
-
-			}
-			else
-			{
-				spdlog::get("logger")->error("[Read sequence] We don't suppoert {} command now.", ikey);
-				//std::exit(EXIT_FAILURE);
-			}
-			//std::string test1 = std::string(item.key());
-
-			//std::cout << data.at("Sequence").at(test1).at("Command") << std::endl;
-			//std::cout << data.at("Sequence").at(test1).at("S (m)") << std::endl;
-
-		}
-		catch (json::exception e)
-		{
-			spdlog::get("logger")->error(e.what());
-			std::exit(EXIT_FAILURE);
-		}
-
-	}
-}
