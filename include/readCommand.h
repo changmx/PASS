cd200d71558c20297ea7922f89b7107f410f7831
changmx <changmx@impcas.ac.cn>
2025-04-02 22:16:22 +0800

    feat：新增对保存command*的vector的排序

--- include/readCommand.h
+++ include/readCommand.h
@@ -3,4 +3,12 @@
 #include "parameter.h"
 #include "particle.h"
 
+#include <vector>
+#include <algorithm>
+#include <tuple>
+
 void read_command_sequence(const Parameter& Para, const std::vector<Bunch>& beam, int input_beamId, std::vector<Command*>& command_vec);
+
+int get_priority(const std::string& name);
+
+void sort_commands(std::vector<Command*>& vec);
