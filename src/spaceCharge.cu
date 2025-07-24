3378bb7fbe0002808b6f3b4d3168f42c7af7f63e
changmx <changmx@impcas.ac.cn>
2025-07-24 11:31:41 +0800

    style：修改类名，增加charge变量，修改aperture为shared_ptr，增加solver的复用

--- src/spaceCharge.cu
+++ src/spaceCharge.cu
@@ -15,14 +15,15 @@ SpaceCharge::SpaceCharge(const Parameter& para, int input_beamId, Bunch& Bunch, 
 	block_x = plan1d.get_blocks_x();
 
 	dev_slice = Bunch.dev_slice_sc;
+	charge = Bunch.charge;
 
 	int Nslice = 0;					// Number of slices
-	FieldSolverMethod fieldSolver;	// Field solver type
+	FieldSolverType solverType;		// Field solver type
 	int Ngridx = 0, Ngridy = 0;		// Number of grid in x and y directions
 	int Nx = 0, Ny = 0;				// Number of grid points in x and y directions
 	double Lx = 0, Ly = 0;			// Length of the grid in x and y directions
 
-	std::unique_ptr<Aperture> aperture;
+	std::shared_ptr<Aperture> aperture;
 	std::string apertureType;
 	int apertureValueSize;
 
@@ -32,18 +33,23 @@ SpaceCharge::SpaceCharge(const Parameter& para, int input_beamId, Bunch& Bunch, 
 
 	try
 	{
+		is_enable_spaceCharge = data.at("Space-charge simulation parameters").at("Is enable space charge");
+		Nslice = data.at("Space-charge simulation parameters").at("Number of slices");
+		solverType = string2Enum(data.at("Space-charge simulation parameters").at("Field solver"));
+
 		s = data.at("Sequence").at(obj_name).at("S (m)");
 		scLength = data.at("Sequence").at(obj_name).at("Length (m)");
 
 		apertureType = data.at("Sequence").at(obj_name).at("Aperture type");
 		apertureValueSize = data.at("Sequence").at(obj_name).at("Aperture value").size();
+
 		if ("Circle" == apertureType)
 		{
 			if (apertureValueSize >= 1)
 			{
 				double radius = data.at("Sequence").at(obj_name).at("Aperture value")[0];
 
-				aperture = std::make_unique<CircleAperture>(radius);
+				aperture = std::make_shared<CircleAperture>(radius);
 			}
 			else
 			{
@@ -60,7 +66,7 @@ SpaceCharge::SpaceCharge(const Parameter& para, int input_beamId, Bunch& Bunch, 
 				half_width = data.at("Sequence").at(obj_name).at("Aperture value")[0];
 				half_height = data.at("Sequence").at(obj_name).at("Aperture value")[1];
 
-				aperture = std::make_unique<RectangleAperture>(half_width, half_height);
+				aperture = std::make_shared<RectangleAperture>(half_width, half_height);
 			}
 			else
 			{
@@ -75,7 +81,7 @@ SpaceCharge::SpaceCharge(const Parameter& para, int input_beamId, Bunch& Bunch, 
 				double hor_semi_axis = data.at("Sequence").at(obj_name).at("Aperture value")[0];
 				double ver_semi_axis = data.at("Sequence").at(obj_name).at("Aperture value")[1];
 
-				aperture = std::make_unique<EllipseAperture>(hor_semi_axis, ver_semi_axis);
+				aperture = std::make_shared<EllipseAperture>(hor_semi_axis, ver_semi_axis);
 			}
 			else
 			{
@@ -89,51 +95,56 @@ SpaceCharge::SpaceCharge(const Parameter& para, int input_beamId, Bunch& Bunch, 
 			std::exit(EXIT_FAILURE);
 		}
 
-
-		is_enable_spaceCharge = data.at("Space-charge simulation parameters").at("Is space charge");
-		Nslice = data.at("Space-charge simulation parameters").at("Number of slices");
-		fieldSolver = data.at("Space-charge simulation parameters").at("Field solver");
-		Ngridx = data.at("Space-charge simulation parameters").at("Number of grid x");
-		Ngridy = data.at("Space-charge simulation parameters").at("Number of grid y");
-		Lx = data.at("Space-charge simulation parameters").at("Grid x length");
-		Ly = data.at("Space-charge simulation parameters").at("Grid y length");
-
-		Nx = Ngridx + 1;
-		Ny = Ngridy + 1;
-
 		double xmin = aperture.get()->get_xmin();
 		double xmax = aperture.get()->get_xmax();
 		double ymin = aperture.get()->get_ymin();
 		double ymax = aperture.get()->get_ymax();
 
-		double left_mesh = -Ngridx / 2.0 * Lx;
-		if (left_mesh > xmin) {
-			double Lx_old = Lx;
-			Lx = (2.0 * std::fabs(xmin)) / Ngridx;
-			spdlog::get("logger")->warn("[SpaceCharge] Hor. min mesh size: {} can't cover aperture: {}, we have changed Lx from: {} to :{}", left_mesh, xmin, Lx_old, Lx);
-		}
+		if (solverType == FieldSolverType::PIC_Conv ||
+			solverType == FieldSolverType::PIC_FD_CUDSS ||
+			solverType == FieldSolverType::PIC_FD_AMGX ||
+			solverType == FieldSolverType::PIC_FD_FFT)
+		{
+			Ngridx = data.at("Sequence").at(obj_name).at("Number of PIC grid x");
+			Ngridy = data.at("Sequence").at(obj_name).at("Number of PIC grid y");
+			Lx = data.at("Sequence").at(obj_name).at("Grid x length");
+			Ly = data.at("Sequence").at(obj_name).at("Grid y length");
 
-		double right_mesh = Ngridx / 2.0 * Lx;
-		if (right_mesh < xmax) {
-			double Lx_old = Lx;
-			Lx = (2.0 * std::fabs(xmax)) / Ngridx;
-			spdlog::get("logger")->warn("[SpaceCharge] Hor. max mesh size: {} can't cover aperture: {}, we have changed Lx from: {} to :{}", right_mesh, xmax, Lx_old, Lx);
-		}
+			Nx = Ngridx + 1;
+			Ny = Ngridy + 1;
 
-		double lower_mesh = -Ngridy / 2.0 * Ly;
-		if (lower_mesh > ymin) {
-			double Ly_old = Ly;
-			Ly = (2.0 * std::fabs(ymin)) / Ngridy;
-			spdlog::get("logger")->warn("[SpaceCharge] Ver. min mesh size: {} can't cover aperture: {}, we have changed Ly from: {} to :{}", lower_mesh, ymin, Ly_old, Ly);
-		}
+			double left_mesh = -Ngridx / 2.0 * Lx;
+			if (left_mesh > xmin) {
+				double Lx_old = Lx;
+				Lx = (2.0 * std::fabs(xmin)) / Ngridx;
+				spdlog::get("logger")->warn("[SpaceCharge] Hor. min mesh size: {} can't cover aperture: {}, we have changed Lx from: {} to :{}", left_mesh, xmin, Lx_old, Lx);
+			}
 
-		double upper_mesh = Ngridy / 2.0 * Ly;
-		if (upper_mesh < ymax) {
-			double Ly_old = Ly;
-			Ly = (2.0 * std::fabs(ymax)) / Ngridy;
-			spdlog::get("logger")->warn("[SpaceCharge] Ver. max mesh size: {} can't cover aperture: {}, we have changed Ly from: {} to :{}", upper_mesh, ymax, Ly_old, Ly);
-		}
+			double right_mesh = Ngridx / 2.0 * Lx;
+			if (right_mesh < xmax) {
+				double Lx_old = Lx;
+				Lx = (2.0 * std::fabs(xmax)) / Ngridx;
+				spdlog::get("logger")->warn("[SpaceCharge] Hor. max mesh size: {} can't cover aperture: {}, we have changed Lx from: {} to :{}", right_mesh, xmax, Lx_old, Lx);
+			}
 
+			double lower_mesh = -Ngridy / 2.0 * Ly;
+			if (lower_mesh > ymin) {
+				double Ly_old = Ly;
+				Ly = (2.0 * std::fabs(ymin)) / Ngridy;
+				spdlog::get("logger")->warn("[SpaceCharge] Ver. min mesh size: {} can't cover aperture: {}, we have changed Ly from: {} to :{}", lower_mesh, ymin, Ly_old, Ly);
+			}
+
+			double upper_mesh = Ngridy / 2.0 * Ly;
+			if (upper_mesh < ymax) {
+				double Ly_old = Ly;
+				Ly = (2.0 * std::fabs(ymax)) / Ngridy;
+				spdlog::get("logger")->warn("[SpaceCharge] Ver. max mesh size: {} can't cover aperture: {}, we have changed Ly from: {} to :{}", upper_mesh, ymax, Ly_old, Ly);
+			}
+		}
+		else if (solverType == FieldSolverType::Eq_Quasi_Static || solverType == FieldSolverType::Eq_Frozen)
+		{
+
+		}
 
 	}
 	catch (json::exception e)
@@ -142,17 +153,39 @@ SpaceCharge::SpaceCharge(const Parameter& para, int input_beamId, Bunch& Bunch, 
 		std::exit(EXIT_FAILURE);
 	}
 
-	picConfig = std::make_shared<PicConfig>(Nx, Ny, Lx, Ly, Nslice, fieldSolver, std::move(aperture));
-
-	auto it = std::find_if(Bunch.picConfig_sc.begin(), Bunch.picConfig_sc.end(), [&](const std::shared_ptr<PicConfig>& p) {
-		return *p == *picConfig;
-		});
-	if (it != Bunch.picConfig_sc.end()) {
-		picConfig = *it;
-	}
-	else
+	if (is_enable_spaceCharge)
 	{
-		Bunch.picConfig_sc.push_back(picConfig);
+		if (solverType == FieldSolverType::PIC_FD_CUDSS)
+		{
+			try {
+				solver = std::make_shared<FieldSolverCUDSS>(Nx, Ny, Lx, Ly, Nslice, aperture);
+			}
+			catch (const std::exception& e) {
+				spdlog::get("logger")->error("[SpaceCharge] Error creating FieldSolver: {} at command = {}, s = {}", e.what(), name, s);
+				std::exit(EXIT_FAILURE);
+			}
+
+			auto it = std::find_if(Bunch.solver_sc.begin(), Bunch.solver_sc.end(), [&](const std::shared_ptr<FieldSolver>& solver_) {
+				return *solver_ == *solver;
+				});
+
+			if (it != Bunch.solver_sc.end()) {
+				solver = *it;
+				size_t solver_index = std::distance(Bunch.solver_sc.begin(), it);
+				spdlog::get("logger")->info("[SpaceCharge] Reusing existing FieldSolver instance (index = {}) at command = {}, s = {}", solver_index, name, s);
+			}
+			else
+			{
+				Bunch.solver_sc.push_back(solver);
+				solver->initialize();
+				spdlog::get("logger")->info("[SpaceCharge] Creating new FieldSolver instance (index = {}) at command = {}, s = {}", Bunch.solver_sc.size() - 1, name, s);
+			}
+
+		}
+		else if (solverType == FieldSolverType::PIC_FD_AMGX)
+		{
+
+		}
 	}
 
 }
@@ -163,10 +196,19 @@ void SpaceCharge::execute(int turn) {
 	callCuda(cudaEventRecord(simTime.start, 0));
 	float time_tmp = 0;
 
+	if (is_enable_spaceCharge)
+	{
+		// Solve Ax=b for space charge
 
-
-
-
+		Np_sur = bunchRef.Np_sur;
+		if (Np_sur <= 0) {
+			spdlog::get("logger")->warn("[SpaceCharge] No particles in the bunch, skipping space charge calculation.");
+			return;
+		}
+		solver->update_b_values(dev_bunch, dev_slice, Np_sur, charge, thread_x, block_x);
+		solver->solve_x_values();
+		solver->calculate_electricField();
+	}
 
 	callCuda(cudaEventRecord(simTime.stop, 0));
 	callCuda(cudaEventSynchronize(simTime.stop));
