94313e7e1bae82a9dfa6b2517ed66767bfb6b473
changmx <changmx@impcas.ac.cn>
2026-01-30 15:07:05 +0800

    style：格式调整，输出内容增加颜色设置，twiss元件标号由从0开始改为从1开始

--- para/generate_smooth_approx_twiss.py
+++ para/generate_smooth_approx_twiss.py
@@ -4,8 +4,8 @@ import sys
 
 def generate_twiss_smooth_approximate(
     circum,
-    mux,
-    muy,
+    Qx,
+    Qy,
     numPoints,
     logi_transfer: str = "drift/matrix/off",
     alfx=0,
@@ -16,85 +16,82 @@ def generate_twiss_smooth_approximate(
     DQy=0,
     muz=0,
 ):
-    betax = circum / (2 * np.pi * mux)
-    betay = circum / (2 * np.pi * muy)
+    betax = circum / (2 * np.pi * Qx)
+    betay = circum / (2 * np.pi * Qy)
 
-    print(f"Circumference (m): {circum}")
-    print(f"Betax = {betax}, Betay = {betay}")
-    print(f"Mux = {mux}, Muy = {muy}")
-    print(f"DQx = {DQx}, DQy = {DQy}")
+    print(f"[Generate \033[34mSmooth Approx.\033[0m Twiss] Circumference (m): {circum}")
+    print(f"[Generate \033[34mSmooth Approx.\033[0m Twiss] Betax = {betax}, Betay = {betay}")
+    print(f"[Generate \033[34mSmooth Approx.\033[0m Twiss] Qx = {Qx}, Qy = {Qy}")
+    print(f"[Generate \033[34mSmooth Approx.\033[0m Twiss] DQx = {DQx}, DQy = {DQy}")
 
-    s_list = [circum / numPoints * i for i in range(numPoints + 1)]
-    mux_list = [mux / numPoints * i for i in range(numPoints + 1)]
-    muy_list = [muy / numPoints * i for i in range(numPoints + 1)]
+    s = np.linspace(0, circum, numPoints, endpoint=True)
+    mux = np.linspace(0, Qx, numPoints, endpoint=True)
+    muy = np.linspace(0, Qy, numPoints, endpoint=True)
 
-    Lattice_json = []
+    elem_dict = {}
 
-    for i in np.arange(numPoints + 1):
+    for i in np.arange(numPoints):
         if i == 0:
-            lattice = {
-                f"TwissSmooApprox_{s_list[i]}[{i}]": {
-                    "S (m)": s_list[i],
-                    "Command": "Twiss",
-                    "S previous (m)": s_list[i],
-                    "Alpha x": alfx,
-                    "Alpha y": alfx,
-                    "Beta x (m)": betax,
-                    "Beta y (m)": betay,
-                    "Mu x": mux_list[i],
-                    "Mu y": muy_list[i],
-                    "Mu z": 0.0,
-                    "Dx (m)": Dx,
-                    "Dpx": Dpx,
-                    "Alpha x previous": alfx,
-                    "Alpha y previous": alfy,
-                    "Beta x previous (m)": betax,
-                    "Beta y previous (m)": betay,
-                    "Mu x previous": mux_list[i],
-                    "Mu y previous": muy_list[i],
-                    "Mu z previous": 0.0,
-                    "Dx (m) previous": Dx,
-                    "Dpx previous": Dpx,
-                    "DQx": 0.0,
-                    "DQy": 0.0,
-                    "Longitudinal transfer": logi_transfer,
-                },
+            elem_dict[f"TwissSmooApprox[{i+1}]"] = {
+                "S (m)": s[i],
+                "Command": "Twiss",
+                "S previous (m)": s[i],
+                "Alpha x": alfx,
+                "Alpha y": alfx,
+                "Beta x (m)": betax,
+                "Beta y (m)": betay,
+                "Mu x": mux[i],
+                "Mu y": muy[i],
+                "Mu z": 0.0,
+                "Dx (m)": Dx,
+                "Dpx": Dpx,
+                "Alpha x previous": alfx,
+                "Alpha y previous": alfy,
+                "Beta x previous (m)": betax,
+                "Beta y previous (m)": betay,
+                "Mu x previous": mux[i],
+                "Mu y previous": muy[i],
+                "Mu z previous": 0.0,
+                "Dx (m) previous": Dx,
+                "Dpx previous": Dpx,
+                "DQx": 0.0,
+                "DQy": 0.0,
+                "Longitudinal transfer": logi_transfer,
             }
-        else:
-            lattice = {
-                f"TwissSmooApprox_{s_list[i]}[{i}]": {
-                    "S (m)": s_list[i],
-                    "Command": "Twiss",
-                    "S previous (m)": s_list[i - 1],
-                    "Alpha x": alfx,
-                    "Alpha y": alfy,
-                    "Beta x (m)": betax,
-                    "Beta y (m)": betay,
-                    "Mu x": mux_list[i],
-                    "Mu y": muy_list[i],
-                    "Mu z": s_list[i] / circum * (muz - 0),
-                    "Dx (m)": Dx,
-                    "Dpx": Dpx,
-                    "Alpha x previous": alfx,
-                    "Alpha y previous": alfy,
-                    "Beta x previous (m)": betax,
-                    "Beta y previous (m)": betay,
-                    "Mu x previous": mux_list[i - 1],
-                    "Mu y previous": muy_list[i - 1],
-                    "Mu z previous": s_list[i - 1] / circum * (muz - 0),
-                    "Dx (m) previous": Dx,
-                    "Dpx previous": Dpx,
-                    "DQx": DQx * (mux_list[i] - mux_list[i - 1]) / mux,
-                    "DQy": DQy * (muy_list[i] - muy_list[i - 1]) / muy,
-                    "Longitudinal transfer": logi_transfer,
-                },
-            }
-        # print(name[i] + "_" + str(s[i]))
-        Lattice_json.append(lattice)
 
-    return Lattice_json, circum
+        else:
+            elem_dict[f"TwissSmooApprox[{i+1}]"] = {
+                "S (m)": s[i],
+                "Command": "Twiss",
+                "S previous (m)": s[i - 1],
+                "Alpha x": alfx,
+                "Alpha y": alfy,
+                "Beta x (m)": betax,
+                "Beta y (m)": betay,
+                "Mu x": mux[i],
+                "Mu y": muy[i],
+                "Mu z": s[i] / circum * (muz - 0),
+                "Dx (m)": Dx,
+                "Dpx": Dpx,
+                "Alpha x previous": alfx,
+                "Alpha y previous": alfy,
+                "Beta x previous (m)": betax,
+                "Beta y previous (m)": betay,
+                "Mu x previous": mux[i - 1],
+                "Mu y previous": muy[i - 1],
+                "Mu z previous": s[i - 1] / circum * (muz - 0),
+                "Dx (m) previous": Dx,
+                "Dpx previous": Dpx,
+                "DQx": DQx * (mux[i] - mux[i - 1]) / Qx,
+                "DQy": DQy * (muy[i] - muy[i - 1]) / Qy,
+                "Longitudinal transfer": logi_transfer,
+            }
+
+    print(f"[Generate \033[34mSmooth Approx.\033[0m Twiss] Success: {len(elem_dict)} smooth approxiamte twiss elements have been generated")
+
+    return elem_dict, circum
 
 
 if __name__ == "__main__":
 
-    pass
+    generate_twiss_smooth_approximate(569.1, 9.47, 9.43, 100, "off", DQx=-1, DQy=-1)
