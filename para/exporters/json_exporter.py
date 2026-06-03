682c28653f125c3830eef2e1d500bb786676938f
changmx <changmx@impcas.ac.cn>
2026-06-03 20:48:50 +0800

    Feat: Add the creation of file paths.

--- para/exporters/json_exporter.py
+++ para/exporters/json_exporter.py
@@ -1,11 +1,16 @@
 import json
+from pathlib import Path
 
 
 class JsonExporter:
 
     @staticmethod
-    def export(data, path):
+    def export(data, output_path):
 
-        with open(path, "w") as f:
+        output_dir = Path(output_path).resolve().parent
+
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        with open(output_path, "w") as f:
 
             json.dump(data, f, indent=4)
