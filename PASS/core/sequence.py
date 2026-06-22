cc2803e2a857cb0259f05cb1af35c529bc03aed3
changmx <changmx@impcas.ac.cn>
2026-06-22 09:44:42 +0800

    feat: add Simulation object to command

--- PASS/core/sequence.py
+++ PASS/core/sequence.py
@@ -1,4 +1,5 @@
 from PASS.commands.command import Command
+from PASS.core.simulation import Simulation
 from PASS.utils.helper import convert_keys_to_lower
 from PASS.utils.logger import set_simple_logging, set_normal_logging, center_string
 
@@ -12,13 +13,13 @@ logger = logging.getLogger(__name__)
 
 class CommandSequence:
 
-    def __init__(self, input_file: str, beam_id: int):
+    def __init__(self, input_file: str, beam_id: int, sim: Simulation):
 
         self.beam_id = beam_id
         self.cmds = []
-        self._load_input(input_file)
+        self._load_input(input_file, sim)
 
-    def _load_input(self, input_file: str):
+    def _load_input(self, input_file: str, sim: Simulation):
         path = Path(input_file)
         if not path.exists():
             raise FileNotFoundError(f"Input file not found: {path}")
@@ -33,7 +34,7 @@ class CommandSequence:
         for cmd_name, cmd_def in data["sequence"].items():
             cmd_def_with_name = cmd_def.copy()
             cmd_def_with_name["name"] = cmd_name
-            cmd = Command.create(self.beam_id, cmd_def_with_name)
+            cmd = Command.create(self.beam_id, cmd_def_with_name, sim)
             self.cmds.append(cmd)
 
         self.num_cmd = len(self.cmds)
