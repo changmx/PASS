faa81629aa5a49035ca5853118eeb28c0debcd26
changmx <changmx@impcas.ac.cn>
2026-07-08 20:20:55 +0800

    feat: change drift to sympletic

--- PASS/commands/element/drift.py
+++ PASS/commands/element/drift.py
@@ -26,6 +26,9 @@ class Drift(Command):
         self.cmd_type = self.__class__.__name__
         self.cmd_name = kwargs["name"]
 
+        if self.length < 0.0:
+            raise ValueError(f"The length of Drift {self.cmd_name} is {self.length}, which should be >= 0")
+
         super().__init__()
 
     def print(self):
@@ -34,50 +37,12 @@ class Drift(Command):
         set_normal_logging()
 
     def execute_cpu(self, sim):
-        L = self.length
-        if np.abs(L) < const.eps:
-            return
+
         beam = sim.beams[self.beam_id]
         bunches: list[BunchInfo] = beam.bunches
 
         for i, bunch in enumerate(bunches):
-            beta = bunch.beta
-            gamma = bunch.gamma
-            circum = bunch.circum
-            start = bunch.start_idx
-            end = bunch.end_idx
-
-            p = beam.particles
-            x = p.x[start:end]
-            px = p.px[start:end]
-            y = p.y[start:end]
-            py = p.py[start:end]
-            z = p.z[start:end]
-            pz = p.pz[start:end]
-            tag = p.tag[start:end]
-
-            r56 = L / (beta**2 * gamma**2)
-
-            c_half = 0.5 * circum
-            mask = (tag > 0).astype(np.float64)
-            """
-            tau = z/beta - ct(=0) = z/beta
-            pt = DeltaE/(P0*c) = beta*DeltaP/P0
-
-            tau0 = dev_particle.z[tid] / beta;
-            pt0 = dev_particle.pz[tid] * beta;
-
-            tau1 = tau0 + (r56 * pt0) * mask;
-            dev_particle.z[tid] = tau1 * beta;
-            """
-            x += L * px * mask
-            y += L * py * mask
-            z += r56 * (pz * beta) * beta * mask
-
-            over = (z > c_half).astype(np.int64)
-            under = (z < -c_half).astype(np.int64)
-
-            z += (under - over) * circum
+            drift_exact_cpu(self.length, beam, bunch)
 
     def execute_gpu(self, sim):
         L = self.length
@@ -102,8 +67,48 @@ class Drift(Command):
             transfer_drift_kernel(
                 (blocks, ),
                 (threads, ),
-                (p.x, p.y, p.z, p.px, p.py, p.pz, p.tag, start, end, beta, gamma, circum, L),
+                (p.x, p.y, p.z, p.px, p.py, p.dp, p.tag, start, end, beta, gamma, circum, L),
             )
+
+
+def drift_exact_cpu(L: float, beam: Beam, bunch: BunchInfo):
+    if np.abs(L) < const.eps:
+        return
+
+    beta0 = bunch.beta
+    gamma0 = bunch.gamma
+    circum = bunch.circum
+    start = bunch.start_idx
+    end = bunch.end_idx
+
+    p = beam.particles
+    x = p.x[start:end]
+    px = p.px[start:end]
+    y = p.y[start:end]
+    py = p.py[start:end]
+    z = p.z[start:end]
+    dp = p.dp[start:end]
+    tag = p.tag[start:end]
+
+    beta = (1 + dp) * (gamma0 * beta0) / np.sqrt(1 + ((1 + dp) * (gamma0 * beta0))**2)
+    pz_sq = (1 + dp)**2 - px**2 - py**2
+    valid = (pz_sq > 0.0) & (tag > 0)
+    tag[~valid] = -1
+    pz_sq_safe = np.maximum(pz_sq, 0.0)
+    pz = np.sqrt(pz_sq_safe)
+
+    c_half = 0.5 * circum
+    mask = (tag > 0).astype(np.float64)
+    L_mask = L * mask
+
+    x += L_mask * (px / pz)
+    y += L_mask * (py / pz)
+    z += L_mask * (1 - (beta0 / beta) * (1 + dp) / pz)
+
+    over = (z > c_half).astype(np.int64)
+    under = (z < -c_half).astype(np.int64)
+
+    z += (under - over) * circum
 
 
 kernel_code = r'''
@@ -114,7 +119,7 @@ void transfer_drift(
     double* __restrict__ z,
     const double* __restrict__ px,
     const double* __restrict__ py,
-    const double* __restrict__ pz,
+    const double* __restrict__ dp,
     const int* __restrict__ tag,
     int start_index,
     int end_index,
@@ -133,7 +138,7 @@ void transfer_drift(
 
     x[i] += L * px[i] * mask;
     y[i] += L * py[i] * mask;
-    z[i] += r56 * (pz[i] * beta) * beta * mask;
+    z[i] += r56 * (dp[i] * beta) * beta * mask;
 
     int over = z[i] > c_half;
     int under = z[i] < -c_half;
