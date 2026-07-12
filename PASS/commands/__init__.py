58337cbde4655e20f128718456ed66c08ff1bb57
changmx <changmx@impcas.ac.cn>
2026-07-12 22:21:33 +0800

    feat: add some elements

--- PASS/commands/__init__.py
+++ PASS/commands/__init__.py
@@ -1,14 +1,17 @@
 from .command import Command
-
 from .twiss import Twiss
-
 from .injection import Injection
-
 from .element.marker import Marker
 from .element.drift import Drift
 from .element.dipole import SBend
+from .element.quadrupole import Quadrupole
+from .element.sextupole import Sextupole
+from .element.octupole import Octupole
+from .element.multipole import Multipole
+from .element.solenoid import Solenoid
+from .element.kicker import Kicker
+from .element.elseparator import ElSeparator
 from .element.exciter import Exciter
-
 from .monitor.statistic import StatMonitor
 
 __all__ = [
@@ -18,6 +21,13 @@ __all__ = [
     "Marker",
     "Drift",
     "SBend",
+    "Quadrupole",
+    "Sextupole",
+    "Octupole",
+    "Multipole",
+    "Solenoid",
+    "Kicker",
+    "ElSeparator",
     "Exciter",
     "StatMonitor",
 ]
