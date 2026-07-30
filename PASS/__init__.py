450e456312b0a009d5725379169f162d1adcecdf
changmx <changmx@impcas.ac.cn>
2026-07-30 16:05:09 +0800

    style: change package name

--- PASS/__init__.py
+++ PASS/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = version("PASS")
+    __version__ = version("pass-sim")
 except PackageNotFoundError:
     __version__ = "unknown"
 
